b71a87a011d4f7d80fec35b2d7f51ab9e8d8a7d1
wenzhili <634449037@qq.com>
2018-09-27 10:21:34 +0800

    网站修改

---
+++ js/imgurl.js
@@ -0,0 +1,55 @@
+var imgList = [{
+	imgurl:'img/1.jpg',
+	desc:'model test picture'
+},{
+	imgurl:'img/2.jpg',
+	desc:'model test picture'
+},{
+	imgurl:'img/3.jpg',
+	desc:'model test picture'
+},{
+	imgurl:'img/4.jpg',
+	desc:'model test picture'
+},{
+	imgurl:'img/5.jpg',
+	desc:'model test picture'
+},{
+	imgurl:'img/6.jpg',
+	desc:'model test picture'
+},{
+	imgurl:'img/7.jpg',
+	desc:'model test picture'
+},{
+	imgurl:'img/9.jpg',
+	desc:'model test picture'
+},{
+	imgurl:'img/10.jpg',
+	desc:'model test picture'
+},{
+	imgurl:'img/11.jpg',
+	desc:'model test picture'
+},{
+	imgurl:'img/12.jpg',
+	desc:'model test picture'
+},{
+	imgurl:'img/13.jpg',
+	desc:'model test picture'
+},{
+	imgurl:'img/14.jpg',
+	desc:'model test picture'
+},{
+	imgurl:'img/15.jpg',
+	desc:'model test picture'
+},{
+	imgurl:'img/16.jpg',
+	desc:'model test picture'
+},{
+	imgurl:'img/17.jpg',
+	desc:'model test picture'
+},{
+	imgurl:'img/18.jpg',
+	desc:'model test picture'
+},{
+	imgurl:'img/19.jpg',
+	desc:'model test picture'
+}]
